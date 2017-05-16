db06f6e0c8183f50fb54f3fcd43beae424fdcfc5
spencer <spencer_canner@hotmail.com>
2017-05-15 21:48:01 -0400

    Add comments and remove unused import

--- classifier.py
+++ classifier.py
@@ -1,5 +1,4 @@
 import os
-import sys
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import svm
@@ -14,35 +13,38 @@ if __name__ == '__main__':
     train_labels = []
     test_labels = []
 
+    # Read in preprocessed data from project directory
     for current_class in classes:
         current_path = os.path.join('./data', current_class)
         for current_file in os.listdir(current_path):
             with open(os.path.join(current_path, current_file), 'r') as open_file:
                 content = open_file.read()
-                if current_file.startswith('cv9'):
+                if current_file.startswith('cv0'):
                     test_data.append(content)
                     test_labels.append(current_class)
                 else:
                     train_data.append(content)
                     train_labels.append(current_class)
 
+    # Create feature vectors for the movie reviews
     vectorizer = TfidfVectorizer(min_df=5, max_df=0.8, sublinear_tf=True, use_idf=True)
     train_vectors = vectorizer.fit_transform(train_data)
     test_vectors = vectorizer.transform(test_data)
     print test_vectors
 
-    # Linear Support Vector Machines
+    # Create and test accuracy of Linear Support Vector Machine classifier
     classifier_svm = svm.LinearSVC()
     classifier_svm.fit(train_vectors, train_labels)
     prediction_svm = classifier_svm.predict(test_vectors)
     print(classification_report(test_labels, prediction_svm))
 
-    # Logistic Regression
+    # Create and test accuracy of Logistic Regression classifier
     classifier_reg = linear_model.LogisticRegression()
     classifier_reg.fit(train_vectors, train_labels)
     prediction_reg = classifier_reg.predict(test_vectors)
     print(classification_report(test_labels, prediction_reg))
 
+    # Create interface to allow new movie review input and sentiment output
     from Tkinter import *
 
     master = Tk()
